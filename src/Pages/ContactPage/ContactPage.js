91b35e457b69bd52c44a3551e3da0dec07c7dc85
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2024-11-22 18:35:48 +0100

    fix: name change

--- src/Pages/ContactPage/ContactPage.js
+++ src/Pages/ContactPage/ContactPage.js
@@ -29,9 +29,9 @@ const ContactPage = () => {
     <div className="general navbarpadding">
       <form className="flex-dir-ver" ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="from_name" />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="from_email" />
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
